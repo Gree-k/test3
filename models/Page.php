eaf838396cd558d6039e3178c5733c6d867a64a0
gree-k <alexandr.greek@mail.ru>
2016-08-07 19:07:29 +0300

    тест

---
+++ models/Page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: YOBA
+ * Date: 07.08.2016
+ * Time: 16:04
+ */
+
+namespace app\models;
+
+
+use yii\db\ActiveRecord;
+
+class Page extends ActiveRecord
+{
+
+}
